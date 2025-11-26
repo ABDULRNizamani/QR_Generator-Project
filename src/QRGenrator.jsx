12206fb16f7408575faff4fe228c4e8b5dc6e216
ARNizamani <yhvvvbk@gmail.com>
2025-11-26 13:36:50 +0500

    fixed pdf

--- src/QRGenrator.jsx
+++ src/QRGenrator.jsx
@@ -174,7 +174,7 @@ function QRGenerator() {
   const uploadPDF = async (file) => {
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', 'qr_code');
+    formData.append('upload_preset', 'qr_pdf_unsigned');
     
     try {
       const response = await fetch('https://api.cloudinary.com/v1_1/dyau5koyt/raw/upload', {
